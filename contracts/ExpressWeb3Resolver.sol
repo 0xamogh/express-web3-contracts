bd13888e65f654a9df6de23197a76195780b5fcb
Amogh <amoghjahagirdargsf@gmail.com>
2022-03-20 17:08:27 +0530

    comments

--- contracts/ExpressWeb3Resolver.sol
+++ contracts/ExpressWeb3Resolver.sol
@@ -19,14 +19,19 @@ contract ExpressWeb3Resolver is ChainlinkClient {
     }
 
     function resolve(bytes calldata ipfsHash) public returns (bytes32 requestId){
-        bytes memory url = "localhost:5001/api/v0/cat?arg="; 
+        //add the default url here
+        bytes memory url = "localhost:5001/api/v0/cat?arg=";
+
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
+        
         request.add("get", string(abi.encodePacked(url, ipfsHash)));
         requestId = sendChainlinkRequestTo(oracle, request, fee);
+        
         hashmap[requestId] = string(ipfsHash);
         return requestId;
     }
 
+    // function overriding here so that user has an option to use custom ipfs node
     function resolve(bytes calldata ipfsHash, bytes calldata ipfsNodeUrl) public returns (bytes32 requestId){
         bytes memory url = abi.encodePacked(ipfsNodeUrl ,"/api/v0/cat?arg="); 
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
@@ -36,10 +41,12 @@ contract ExpressWeb3Resolver is ChainlinkClient {
         return requestId;
     }
 
+    // this gets called by the oracle and the data is received here
     function fulfill(bytes32 request_id, string calldata _md5hash) public recordChainlinkFulfillment(request_id) {
         hashmap[request_id] = _md5hash;
     }
 
+    // here is where the recieved data is verified for the api
     function resolveCallback(bytes32 request_id, string calldata md5) public view returns (bool verified){
         // should add check to see if the request is completed/pending/errored out
         return keccak256(bytes(hashmap[request_id])) == keccak256(bytes(md5));
